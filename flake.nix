ba0e6b217926c8871fa71a753a8990c0929c63d6
Frank Chiarulli <fcjr@Franks-MacBook-Pro.local>
2024-11-20 21:05:10 -0500

    setup brew and mas

--- flake.nix
+++ flake.nix
@@ -5,17 +5,61 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [ 
+          pkgs.mkalias
           pkgs.neovim
+          pkgs.tmux
+
+          pkgs.wezterm
+          pkgs.obsidian
         ];
+
+      homebrew = {
+        enable = true;
+	brews = [
+          "mas"
+	];
+	casks = [
+          "firefox@developer-edition"   
+	];
+        masApps = {
+	  "Bitwarden" = 1352778147;
+	  "Pure Paste" = 1611378436;
+	};
+        onActivation.cleanup = "zap";
+      };
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -38,7 +82,15 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."default" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew {
+          nix-homebrew = {
+            enable = true;
+	    user   = "fcjr";
+          };
+	}
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
